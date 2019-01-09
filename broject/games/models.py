93b0d868bbf34592f7d8a4cd319a73f99375da5a
Nick <nicholas.colb@gmail.com>
2019-01-09 15:07:04 +0200

    Added admin access to db

--- broject/games/models.py
+++ broject/games/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Game(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     price = models.IntegerField
