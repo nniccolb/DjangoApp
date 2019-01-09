86b5e6233fdb320e8ab09017d1aa53fce781fd60
Jonatan Lygdman <lygdmaj1@aalto.fi>
2019-01-09 14:00:05 +0200

    Added first models

--- broject/games/models.py
+++ broject/games/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Game(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    creator = models.CharField(max_length=100)
+    source = models.CharField(max_length=500)
