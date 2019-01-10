6d2ffea0519b83674364d7be64b8acbc637c3043
Jonatan Lygdman <lygdmaj1@aalto.fi>
2019-01-10 11:29:23 +0200

    Added single game view

--- broject/games/views.py
+++ broject/games/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>hey dude</h1>")
+
+def singleView(request, game_id):
+    return HttpResponse("<h1>Single game view for game #" + str(game_id) + "</h1>")
