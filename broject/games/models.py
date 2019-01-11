3d6a94dd0f79ae883fe6c6bfacca2ce41bd586e0
Akseli Paukkeri <akseli.paukkeri@aalto.fi>
2019-01-11 11:02:18 +0200

    made a game and categoryview

--- broject/games/models.py
+++ broject/games/models.py
@@ -8,7 +8,9 @@ class Category(models.Model):
 
 class Game(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    price = models.IntegerField
+    price = models.IntegerField(default=0)
     title = models.CharField(max_length=100)
     source = models.CharField(max_length=500)
-    
+
+    def __str__(self):
+        return self.title
--- broject/games/views.py
+++ broject/games/views.py
@@ -1,14 +1,16 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 from .models import Category
 
 def index(request):
     all_categories = Category.objects.all()
-    template = loader.get_template('games/index.html')
-    context = {
-        'all_categories': all_categories
-    }
-    return HttpResponse(template.render(context, request))
+
+    return render(request, 'games/index.html',{'all_categories': all_categories})
 
 def categoryView(request, category_id):
-    return HttpResponse("<h1>Single category view for caregory #" + str(category_id) + "</h1>")
+    #return HttpResponse("<h1>Single category view for caregory #" + str(category_id) + "</h1>")
+    try:
+        category = Category.objects.get(pk=category_id)
+    except Category.DoesNotExist:
+        raise Http404("category not exist")
+    return render(request, 'games/categoryView.html',{'category': category})
