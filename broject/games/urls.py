fe0cadda6d80ece7468c7931e43e8a0ad2d33538
Jonatan Lygdman <lygdmaj1@aalto.fi>
2019-01-16 12:50:56 +0200

    Created form for adding games

--- broject/games/urls.py
+++ broject/games/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path
 from . import views
+
+app_name = 'games'
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<pk>/', views.DetailView.as_view(), name='categoryView'),
-    path('<pk>/add/', views.GameCreate.as_view(), name='game-add'),
+    path('/add/', views.GameCreate.as_view(), name='game-add'),#Muuta tää kun saadaan view peleille
     path('<int:category_id>/favorite', views.favorite, name='favorite'),
 ]
