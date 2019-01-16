ee461015a37da6bbb5ab55d781ecb22bcafe2799
Jonatan Lygdman <lygdmaj1@aalto.fi>
2019-01-16 12:59:33 +0200

    Deleted favorite functionality

--- broject/games/urls.py
+++ broject/games/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<pk>/', views.DetailView.as_view(), name='categoryView'),
     path('/add/', views.GameCreate.as_view(), name='game-add'),#Muuta tää kun saadaan view peleille
-    path('<int:category_id>/favorite', views.favorite, name='favorite'),
 ]
--- broject/games/views.py
+++ broject/games/views.py
@@ -16,20 +16,6 @@ class DetailView(generic.DetailView):
     model = Category
     template_name = 'games/categoryView.html'
 
-def favorite(request, category_id):
-    category = get_object_or_404(Category, pk=category_id)
-    try:
-        selected_game = category.game_set.get(pk=request.POST['game'])
-    except (KeyError, Game.DoesNotExist):
-        return render(request, 'games/categoryView.html', {
-        'category': category,
-        'error_message': "You did not select a valid game",
-        })
-    else:
-        selected_game.favorite = True
-        selected_game.save()
-        return render(request, 'games/categoryView.html',{'category': category})
-
 class GameCreate(CreateView):
     model = Game
     fields = ['category', 'price', 'title', 'source']
