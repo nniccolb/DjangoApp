dca8b4cc75dc162db9513510ca1f9eecd815baab
Jonatan Lygdman <lygdmaj1@aalto.fi>
2019-01-16 15:57:32 +0200

    Added login form

--- broject/games/urls.py
+++ broject/games/urls.py
@@ -6,6 +6,7 @@ app_name = 'games'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('login/', views.LoginView.as_view(), name='loginView'),
     path('<pk>/', views.DetailView.as_view(), name='categoryView'),
     path('/add/', views.GameCreate.as_view(), name='game-add'),#Muuta tää kun saadaan view peleille
 ]
--- broject/games/views.py
+++ broject/games/views.py
@@ -1,8 +1,11 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Game
 from django.views import generic
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
+from django.contrib.auth import login, authenticate
+from django.views.generic import View
+from .forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -19,3 +22,30 @@ class DetailView(generic.DetailView):
 class GameCreate(CreateView):
     model = Game
     fields = ['category', 'price', 'title', 'source']
+
+class LoginView(View):
+    form_class = UserForm
+    template_name = 'games/login_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('games:index')
+
+        return render(request, self.template_name, {'form': form})
