de6e0836df4ec082c3eb6872139f63d34fb312d4
Jonatan Lygdman <lygdmaj1@aalto.fi>
2019-02-01 15:11:48 +0200

    Fixed bug in file

--- broject/games/models.py
+++ broject/games/models.py
@@ -23,23 +23,11 @@ class Game(models.Model):
     source = models.CharField(max_length=500)
     
     image = models.CharField(max_length=500)
-    developer = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
+    developer = models.ForeignKey('UserProfile', on_delete=models.PROTECT)
 
-
-
-    developer = models.ForeignKey('UserProfile',on_delete=models.PROTECT)
 
     def get_absolute_url(self):
         return reverse('games:index')
 
     def __str__(self):
         return self.title
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    userType = models.CharField(max_length=100)
-    games = models.ManyToManyField(Game)
-
-
-    def __str__(self):
-        return self.userType
