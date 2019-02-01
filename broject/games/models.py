893d766f8bd46f3944309bba837f74e5eb86a1bf
Nick <nicholas.colb@gmail.com>
2019-02-01 12:06:37 +0200

    Added UserProfile-Game many to many relatioship and added conditional rendering for the iFrame

--- broject/games/models.py
+++ broject/games/models.py
@@ -8,22 +8,25 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    userType = models.CharField(max_length=100)
+    games = models.ManyToManyField('Game')
+
+    def __str__(self):
+        return self.userType
+
 class Game(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     price = models.IntegerField(default=0)
     title = models.CharField(max_length=100)
     source = models.CharField(max_length=500)
     image = models.CharField(max_length=500)
+    developer = models.ForeignKey(UserProfile, on_delete=models.PROTECT)
+
 
     def get_absolute_url(self):
         return reverse('games:index')
 
     def __str__(self):
         return self.title
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    userType = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.userType
