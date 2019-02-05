21f70dba623eb4b2a36ffc5173ed160485bf7a55
Jonatan Lygdman <lygdmaj1@aalto.fi>
2019-02-05 10:50:24 +0200

    Comparing current user score with high score through js

--- broject/games/views.py
+++ broject/games/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login, authenticate
 from django.views.generic import View
 from .forms import UserForm
 from hashlib import md5
+from django.core import serializers
 
 
 class IndexView(generic.ListView):
@@ -20,10 +21,12 @@ class DetailView(generic.DetailView):
     model = Category
     pk_url_kwarg='category_pk'
     template_name = 'games/categoryView.html'
+
 class GameView(generic.DetailView):
     model = Game
     pk_url_kwarg='game_pk'
     template_name = 'games/gameView.html'
+
 class GameCreate(CreateView):
     model = Game
     fields = ['category', 'price', 'title', 'source', 'image']
